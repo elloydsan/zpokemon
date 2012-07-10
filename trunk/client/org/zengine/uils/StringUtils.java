31ab0639068d663bd62f3dda1e357da1f552e80c
s118428@student.hb.se <s118428@student.hb.se@8b6356c4-8455-4ce6-c427-45d682c52303>
2012-07-10 18:11:19 +0000

    Made a class for string manipulation.

--- trunk/client/org/zengine/uils/StringUtils.java
+++ trunk/client/org/zengine/uils/StringUtils.java
@@ -1,13 +1,44 @@
 package org.zengine.uils;
 
+/**
+ * This class provides various methods for string manipulation.
+ * 
+ * @author Kim
+ *
+ */
 public class StringUtils {
 	
+	/**
+	 * Removes all whitespace characters such as spaces, tabs etc.
+	 */
 	
-	
+	public static String removeWhitespace(String text) {
+		return text.replaceAll("\\s", "");
+	}
 	
 	/**
-	 * ADD STRING MANIPULATION SHIT HERE
+	 * Removes all digit characters from a string.
 	 */
+	
+	public static String removeDigits(String text) {
+		return text.replaceAll("\\d", "");
+	}
+	
+	/**
+	 * This method act as a backspace would. When called, it returns
+	 * the string with it's last character removed.
+	 */
+	
+	public static String backspace(String text) {
+		
+		if(text == null)
+			return null;
+		else if(text.length() < 2)
+			return "";
+		else
+			return text.substring(0, text.length()-1);	
+	}
+	
 	
 
 }
